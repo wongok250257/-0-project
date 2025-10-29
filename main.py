ab62e4072acea2771235f2379a0db59de41cb621
wongok250257 <wongok.s250257@ggh.goe.go.kr>
2025-10-29 19:48:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ st.title('좋아하는 사람이 있나요?')
 name = st.text_input('이름을 입력해주세요 : ')
 menu = st.selectbox('좋아하는 사람이 있나요?:', ['좋아하는 사람O','좋아하는 사람X'])
 if st.button('인사말') : 
-  st.write(name+'님은 '+menu+ '이시군요 잘 부탁드려요')
+  st.write(name+'님은 '+menu+'이시군요 잘 부탁드려요')
