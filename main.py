d2fc49ce8cbad1f629ef0c704be2fc2a46db49c4
wongok250257 <wongok.s250257@ggh.goe.go.kr>
2025-10-29 20:02:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import streamlit as st
 def main_page():
     st.header('좋아하는 사람이 있으신 분들의 페이지')
     st.image('https://i.namu.wiki/i/DiYExkj1wNJiwl8yxJ_35zdhCk_Zks9GtCa8zqErfpH703Sv7DCcJeeLBBMxLC-eofYKMz3e8w2nHa5uY4bZ4V5nWh74TXtbYnzyHQ5WP2iXQBgTsscz9fxdPeV3jIMlLIz-Q_rsR0wLT8WIHg6t1Q.webp')
-    iris = 좋아하는 사람이 있으신 당신을 제가 도와드릴게요!
+    iris = pd.read_csv('좋아하는 사람이 있으신 당신을 제가 도와드릴게요!')
     st.write(iris)
     
 def page(2):
     st.header('좋아하는 사람이 없으신 분들의 페이지')
     st.image('https://blog.kakaocdn.net/dna/GHQUw/btr35vWC5ng/AAAAAAAAAAAAAAAAAAAAAFWKhDDyeFtlcOIFvUAEwRezSUEhs-5eoOymt5CbnATF/img.jpg?credential=yqXZFxpELC7KVnFOS48ylbz2pIh7yKj8&amp;expires=1761922799&amp;allow_ip=&amp;allow_referer=&amp;signature=rey%2Bqe%2FNNc3AlVaQte7nh%2F52Ekc%3D')
-    iris2 = 당신의 앞날에 희망이 있기를 바랄게요!
+    iris2 = pd.read_csv('당신의 앞날에 희망이 있기를 바랄게요!')
     st.write(iris2)
