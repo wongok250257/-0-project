6bfa3401fa419c353c40aeaa15dcad2281e61014
wongok250257 <wongok.s250257@ggh.goe.go.kr>
2025-10-29 19:35:31 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random # import 함수꾸러미이름
-print(random.choice(['찍먹','부먹'])) # a.b
-
-import random as rd # import 함수꾸러미이름 as 별명
-print(rd.choice(['찍먹','부먹']))
-
-from random import choice # from 함수꾸러미이름 import 함수이름
-print(choice(['찍먹','부먹']))
+import streamlit as st
+st.title('좋아하는 사람이 있나요?')
+name = st.text_input('이름을 입력해주세요 : ')
+menu = st.selectbox('좋아하는 사람이 있나요?:', ['좋아하는 사람O','좋아하는 사람X'])
+if st.button('인사말') : 
+  st.write(name+'님은 '+menu+ '이시군요 잘 부탁드려요')
